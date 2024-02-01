a78c814a0896ea948700b5facff9ebfcce16de79
Rhuan-Campos <rhuanpzo@gmail.com>
2024-02-01 16:55:34 -0300

    feat: Added EmployeeDto and Mapper

---
+++ project-backend/src/main/java/io/rhuancampos/projectbackend/dto/EmployeeDto.java
@@ -0,0 +1,9 @@
+package io.rhuancampos.projectbackend.dto;
+
+public class EmployeeDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
---
+++ project-backend/src/main/java/io/rhuancampos/projectbackend/dto/mapper/EmployeeMapper.java
@@ -0,0 +1,19 @@
+package io.rhuancampos.projectbackend.dto.mapper;
+
+package io.rhuancampos.projectbackend.dto.mapper;
+
+import io.rhuancampos.projectbackend.dto.EmployeeDto;
+import io.rhuancampos.projectbackend.entity.Employee;
+import org.modelmapper.ModelMapper;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto toEmployeeDto(Employee employee) {
+        return new ModelMapper().map(employee, EmployeeDto.class);
+    }
+
+    public static Employee toEmployee(EmployeeDto employeeDto) {
+        return new ModelMapper().map(employeeDto, Employee.class);
+    }
+}
+
